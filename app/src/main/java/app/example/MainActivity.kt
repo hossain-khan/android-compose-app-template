e5d7e866037d158c5d76bec44714f9c32623137b
Hossain Khan <hello@hossain.dev>
2025-07-05 20:46:51 -0400

    [APPLIED] Changes based on `https://patch-diff.githubusercontent.com/raw/slackhq/circuit/pull/2168.diff`

--- app/src/main/java/app/example/MainActivity.kt
+++ app/src/main/java/app/example/MainActivity.kt
@@ -9,7 +9,6 @@ import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.runtime.remember
 import app.example.circuit.InboxScreen
 import app.example.di.ActivityKey
-import dev.zacsweers.metro.AppScope
 import app.example.ui.theme.CircuitAppTheme
 import com.slack.circuit.backstack.rememberSaveableBackStack
 import com.slack.circuit.foundation.Circuit
@@ -19,45 +18,46 @@ import com.slack.circuit.foundation.rememberCircuitNavigator
 import com.slack.circuit.overlay.ContentWithOverlays
 import com.slack.circuit.sharedelements.SharedElementTransitionLayout
 import com.slack.circuitx.gesturenavigation.GestureNavigationDecorationFactory
-import dev.zacsweers.metro.ContributesBinding
-import javax.inject.Inject
+import dev.zacsweers.metro.AppScope
+import dev.zacsweers.metro.ContributesIntoMap
+import dev.zacsweers.metro.Inject
+import dev.zacsweers.metro.binding
 
-@ContributesBinding(AppScope::class, boundType = Activity::class)
+@ContributesIntoMap(AppScope::class, binding = binding<Activity>())
 @ActivityKey(MainActivity::class)
-class MainActivity
-    @Inject
-    constructor(
-        private val circuit: Circuit,
-    ) : ComponentActivity() {
-        @OptIn(ExperimentalSharedTransitionApi::class)
-        override fun onCreate(savedInstanceState: Bundle?) {
-            enableEdgeToEdge()
-            super.onCreate(savedInstanceState)
+@Inject
+class MainActivity(
+    private val circuit: Circuit,
+) : ComponentActivity() {
+    @OptIn(ExperimentalSharedTransitionApi::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
 
-            setContent {
-                CircuitAppTheme {
-                    // See https://slackhq.github.io/circuit/navigation/
-                    val backStack = rememberSaveableBackStack(root = InboxScreen)
-                    val navigator = rememberCircuitNavigator(backStack)
+        setContent {
+            CircuitAppTheme {
+                // See https://slackhq.github.io/circuit/navigation/
+                val backStack = rememberSaveableBackStack(root = InboxScreen)
+                val navigator = rememberCircuitNavigator(backStack)
 
-                    // See https://slackhq.github.io/circuit/circuit-content/
-                    CircuitCompositionLocals(circuit) {
-                        // See https://slackhq.github.io/circuit/shared-elements/
-                        SharedElementTransitionLayout {
-                            // See https://slackhq.github.io/circuit/overlays/
-                            ContentWithOverlays {
-                                NavigableCircuitContent(
-                                    navigator = navigator,
-                                    backStack = backStack,
-                                    decoratorFactory =
-                                    remember(navigator) {
-                                        GestureNavigationDecorationFactory(onBackInvoked = navigator::pop)
-                                    },
-                                )
-                            }
+                // See https://slackhq.github.io/circuit/circuit-content/
+                CircuitCompositionLocals(circuit) {
+                    // See https://slackhq.github.io/circuit/shared-elements/
+                    SharedElementTransitionLayout {
+                        // See https://slackhq.github.io/circuit/overlays/
+                        ContentWithOverlays {
+                            NavigableCircuitContent(
+                                navigator = navigator,
+                                backStack = backStack,
+                                decoratorFactory =
+                                remember(navigator) {
+                                    GestureNavigationDecorationFactory(onBackInvoked = navigator::pop)
+                                },
+                            )
                         }
                     }
                 }
             }
         }
     }
+}
--- app/src/main/java/app/example/di/CircuitModule.kt
+++ app/src/main/java/app/example/di/CircuitModule.kt
@@ -5,19 +5,23 @@ import com.slack.circuit.runtime.presenter.Presenter
 import com.slack.circuit.runtime.ui.Ui
 import dev.zacsweers.metro.AppScope
 import dev.zacsweers.metro.ContributesTo
-import dev.zacsweers.metro.SingleIn
+import dev.zacsweers.metro.Multibinds
 import dev.zacsweers.metro.Provides
+import dev.zacsweers.metro.SingleIn
 
 /**
  * Metro providers that contribute Circuit dependencies.
  */
 @ContributesTo(AppScope::class)
 interface CircuitModule {
-    /**
-     * Provides a singleton instance of Circuit with presenter and ui configured.
-     */
-    @SingleIn(AppScope::class)
+    @Multibinds
+    fun presenterFactories(): Set<Presenter.Factory>
+
+    @Multibinds
+    fun uiFactories(): Set<Ui.Factory>
+
     @Provides
+    @SingleIn(AppScope::class)
     fun provideCircuit(
         presenterFactories: Set<Presenter.Factory>,
         uiFactories: Set<Ui.Factory>,
